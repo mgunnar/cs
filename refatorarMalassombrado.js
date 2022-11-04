be6b2b82b8796417789ad0b43004f4cd5632ddec
Matheusm <devmatheus@gmail.com>
2022-11-04 00:11:10 -0300

    outras alterações.

--- refatorarMalassombrado.js
+++ refatorarMalassombrado.js
@@ -2,17 +2,20 @@ function solution(matrix) {
   let matrixColumn = matrix.length;
   let matrixElementsRow = matrix[0].length;
   let sumMatrix = 0;
+  
   for (let x = 0; x < matrixColumn; x++) {
     for (let y = 0; y < matrixElementsRow; y++) {
-      if (x == 0 && matrix[x][y] != 0) {
-        console.log("Somou " + matrix[x][y]);
-        sumMatrix += matrix[x][y];
+      
+      if (matrix[y][x] === 0) {
+        break;        
+        }
+      sumMatrix += matrix[x][y];
       }
-      if (x >= 2 && matrix[x - 1][y] == 0){
-        console.log("Subtraiu " + matrix[x][y]);
-        sumMatrix -= matrix[x][y];
-      }
+      
     }
+    return sumMatrix;
   }
-  return sumMatrix;
-}
+
+  console.log(solution([[1, 0, 0, 2, 5], 
+                        [0, 6, 0, 2, 1], 
+                        [3, 2, 1, 3, 10]]));
